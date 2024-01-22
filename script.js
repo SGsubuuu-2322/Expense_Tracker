0de6d6d56aecd7d0d8425c2cbe17d95325d20f60
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-22 21:19:02 +0530

    Added the scripts for showing transaction list to the DOM...

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+
+const dummyTransactions = [
+  { id: 1, text: "Flower", amount: -20 },
+  { id: 2, text: "Salary", amount: 300 },
+  { id: 3, text: "Book", amount: -10 },
+  { id: 4, text: "Camera", amount: 150 },
+];
+
+let transactions = dummyTransactions;
+
+function addTransactionToDom(transaction) {
+  let sign = transaction.amount > 0 ? "+" : "-";
+
+  const item = document.createElement("li");
+  item.classList.add(transaction.amount > 0 ? "plus" : "minus");
+  item.innerHTML = ` 
+        ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn">X</button>
+    `;
+
+  list.appendChild(item);
+}
+
+function init() {
+  list.innerHTML = "";
+  transactions.forEach(addTransactionToDom);
+}
+
+init();
