29f399c3d6c7c99e8d14fa18b16f34fadb6a0c6a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-22 23:19:03 +0530

    Added the scripts for showing the updated value...

--- script.js
+++ script.js
@@ -29,9 +29,29 @@ function addTransactionToDom(transaction) {
   list.appendChild(item);
 }
 
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const totalBalance = amounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item)) * -1
+  ).toFixed(2);
+
+  balance.innerText = `$${totalBalance}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
 function init() {
   list.innerHTML = "";
   transactions.forEach(addTransactionToDom);
+  updateValues();
 }
 
 init();
